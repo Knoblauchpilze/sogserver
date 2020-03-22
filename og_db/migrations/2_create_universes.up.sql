938196e0a40b01a05b447c6ec5a18cc0bdb0a46f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2020-03-22 15:26:51 +0100

    Refined parameters to create a universe

--- og_db/migrations/2_create_universes.up.sql
+++ og_db/migrations/2_create_universes.up.sql
@@ -2,8 +2,15 @@
 -- Create the table defining universes.
 CREATE TABLE universes (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    name text,
+    name text NOT NULL,
     created_at timestamp with time zone default current_timestamp,
+    economic_speed integer NOT NULL,
+    fleet_speed integer NOT NULL,
+    research_speed integer NOT NULL,
+    fleet_to_ruins_ratio numeric(4,2) NOT NULL,
+    defense_to_ruins_ratio numeric(4,2) NOT NULL default 0,
+    galaxy_count integer NOT NULL,
+    solar_system_size integer NOT NULL,
     PRIMARY KEY (id)
 );
 

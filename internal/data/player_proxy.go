cb2355237d7753fe3133957a686e7364b85f298a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2021-04-04 17:49:44 +0200

    feat: No error returned in case no players are fetched by the DB request

--- internal/data/player_proxy.go
+++ internal/data/player_proxy.go
@@ -104,10 +104,6 @@ func (p *PlayerProxy) Players(filters []db.Filter) ([]game.Player, error) {
 		players = append(players, pla)
 	}
 
-	if len(players) == 0 {
-		return players, game.ErrElementNotFound
-	}
-
 	return players, nil
 }
 
